8ded9ec33f3a527f00833e03ad6cb295db4e64d0
steve <usoni1@asu.edu>
2019-11-13 12:58:48 -0700

    getting graphs for analysis

--- Evaluation/paper_graphs/create_graphs.py
+++ Evaluation/paper_graphs/create_graphs.py
@@ -5,9 +5,8 @@ import pprint as pp
 
 # define all the global variables here
 # feature feedback, random_sampling_enabled, include_discovery, include_gain, include_feature_distinguishing, include_prob_term
-# it shouldn't exceed 10
-graph_settings = [
-    [1, 1, 1, 0, 1, 1],
+required_graph_settings = [
+    [1, 1, 1, 1, 1, 1],
     [1, 0, 1, 1, 1, 1],
     [1, 0, 1, 0, 1, 1],
 ]
@@ -26,6 +25,12 @@ case_settings = [
 
 class create_graphs:
     def __init__(self):
+        self.graph_data = None
+        self.extract_relevant_data()
+        self.create_graphs()
+
+    def extract_relevant_data(self):
+        self.graph_data = []
         # gather the data for all the cases
         with open(file_name, 'r') as data_file:
             data = data_file.read()
@@ -33,37 +38,39 @@ class create_graphs:
             for single_case in cases:
                 # extract case settings
                 single_case = single_case[0]
+                # given_case_setting
+                given_case_settings = []
                 for single_case_setting in case_settings:
                     truth_value = re.findall(r"%s\s*=\s*\w+" % single_case_setting, single_case)[0]
                     if truth_value.find("True") != -1:
-                        print("true")
+                        given_case_settings.append(1)
                     elif truth_value.find("False") != -1:
-                        print("false")
+                        given_case_settings.append(0)
                     else:
                         print("unknown truth value")
-        # create graph for all the cases
 
+                # print(given_case_settings)
 
-    def make_graph(self, bayes, mle, file_name):
-        save_file_name = file_name[:file_name.index('.result')] + ".png"
-        folder = './graphs/'
-        bayes = np.array(bayes)
-        mle = np.array(mle)
+                # now gather the data here.
+                pre = "REGION BAYES ERROR LIST"
+                post = "INTERESTING"
+                case_data = eval(re.search('%s((.|\n)*)%s' % (pre, post), single_case).groups(1)[0])
+                # print(case_data)
 
-        bayes_errors = np.abs(bayes[:, 0] - bayes[:, 2])
-        mle_errors = np.abs(mle[:, 0] - mle[:, 1])
+                if given_case_settings in required_graph_settings:
+                    self.graph_data.append(case_data)
+
+    def create_graphs(self):
+        # save_file_name = "basic.png"
+        # folder = './graphs/'
+        x = np.arange(len(self.graph_data[0]))
 
         plt.figure()
-        plt.ylim([0, np.max([np.max(bayes_errors), np.max(mle_errors)])])
-        x, y = (bayes[:, 0], bayes_errors)
-        plt.scatter(x, y, c='tab:blue', label='bayes', alpha=0.3, edgecolors='none')
-        plt.savefig(folder + "bayes_" + save_file_name)
-
-        plt.figure()
-        plt.ylim([0, np.max([np.max(bayes_errors), np.max(mle_errors)])])
-        x, y = (mle[:, 0], mle_errors)
-        plt.scatter(x, y, c='tab:red', label='mle', alpha=0.3, edgecolors='none')
-        plt.savefig(folder + "mle_" + save_file_name)
+        for data in self.graph_data:
+            plt.plot(x, data)
+            plt.legend(required_graph_settings, loc='upper right')
+        plt.show()
+        # plt.savefig(folder + "mle_" + save_file_name)
 
 if __name__ == "__main__":
     print("Printing Graphs for the paper, main and supplemental section")
